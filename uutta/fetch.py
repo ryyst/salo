ab9ace9fc6a96ae8458ebc0f1a7bab05899a93df
ryyst <ryyst@tuta.io>
2025-09-15 20:22:05 +0300

    fix: don't cache news files
    Prevented updating more often than once per day.

--- uutta/fetch.py
+++ uutta/fetch.py
@@ -1,11 +1,8 @@
-from utils.cache import cache_output
-
 from .config import UuttaConfig
 from .schema import RawRSSData
 from .api import RSSFetcher
 
 
-@cache_output("uutta_sss_raw", RawRSSData)
 def fetch_sss_rss(params: UuttaConfig) -> RawRSSData:
     """Fetch articles from SSS.fi RSS feed."""
     fetcher = RSSFetcher(params.sss_rss_url)
@@ -13,7 +10,6 @@ def fetch_sss_rss(params: UuttaConfig) -> RawRSSData:
     return RawRSSData(articles=articles)
 
 
-@cache_output("uutta_salo_raw", RawRSSData)
 def fetch_salo_rss(params: UuttaConfig) -> RawRSSData:
     """Fetch articles from Salo tiedotteet RSS feed."""
     fetcher = RSSFetcher(params.salo_rss_url)
